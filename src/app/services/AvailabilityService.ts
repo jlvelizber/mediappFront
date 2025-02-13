23983cb3a2f9ae649e9e8622f9669250dfa22760
Jorge Veliz <jorgeconsalvacion@gmail.com>
2025-02-13 18:46:27 -0500

    change routes availabilitys for doctor role

--- src/app/services/AvailabilityService.ts
+++ src/app/services/AvailabilityService.ts
@@ -5,14 +5,14 @@ import { apiClient } from "./api";
 export const AvailabilityService = {
 
     getMyAvailability: async (): Promise<DoctorAvailabilityInterface[]> => {
-        const response = await apiClient.get("/my-availabilities");
+        const response = await apiClient.get("/availabilities");
         const { data } = await response.data;
         return data;
     },
 
 
     saveAvailability: async (availability: DoctorAvailabilityInterface): Promise<DoctorAvailabilityInterface> => {
-        const response = await apiClient.post(`/save-my-availability`, availability);
+        const response = await apiClient.post(`/availabilities`, availability);
         const { data } = await response.data;
         return data;
     },
