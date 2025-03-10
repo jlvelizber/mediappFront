2126802d5756d49c5ac6ada95f58b00d18853ef2
Jorge Veliz <jorgeconsalvacion@gmail.com>
2025-03-10 11:20:57 -0500

    style: change table style

--- src/app/components/AvailabilityList/AvailabilityList.tsx
+++ src/app/components/AvailabilityList/AvailabilityList.tsx
@@ -6,23 +6,18 @@ import { AvailabilityListInterface } from './AvailabilityListInterface'
 export const AvailabilityList: FC<AvailabilityListInterface> = ({ items, onRemove }) => {
 
     return (
-        <Table>
-            <thead>
-                <tr>
-                    <th>Disponibilidad</th>
-                    <th>Acciones</th>
+        <Table headers={['Disponibilidad', 'Acciones']} shadow>
+            {items.map((availability: DoctorAvailabilityInterface) => (
+                <tr key={availability.id} className="hover:bg-gray-100 transition">
+                    <td className="px-6 py-2 text-gray-700 w-full">
+                        <span className="font-medium"> {availability.day_of_week} </span> - {availability.start_time} hasta {availability.end_time}
+                    </td>
+                    <td className="px-6 py-2 w-full flex mx-auto">
+                        <button className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-lg shadow-sm transition" onClick={() => availability.id !== undefined && onRemove(availability.id)}> Eliminar</button>
+                    </td>
                 </tr>
-            </thead>
-            <tbody>
-                {items.map((availability: DoctorAvailabilityInterface) => (
-                    <tr key={availability.id}>
-                        <td>
-                            {availability.day_of_week} - {availability.start_time} hasta {availability.end_time}
-                        </td>
-                        <td> <button className="bg-red-500 text-white px-4 py-2 rounded" onClick={() => availability.id !== undefined && onRemove(availability.id)}> Eliminar</button></td>
-                    </tr>
-                ))}
-            </tbody>
+            ))}
+
         </Table>
 
     )
