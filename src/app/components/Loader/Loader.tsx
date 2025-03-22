74abf0e6aafd5eaa9b8c9054789682ab2382130a
Jorge Veliz <jorgeconsalvacion@gmail.com>
2025-03-22 17:53:52 -0500

    style: Loader, add default prop for loader in all page

--- src/app/components/Loader/Loader.tsx
+++ src/app/components/Loader/Loader.tsx
@@ -1,10 +1,11 @@
 
 interface LoaderProps {
     message?: string
+    onScreen?: boolean
 }
-export default function Loader({ message }: LoaderProps) {
+export default function Loader({ message, onScreen = true }: LoaderProps) {
     return (
-        <div className="fixed inset-0 flex items-center justify-center bg-white/80 z-50">
+        <div className={`${onScreen && 'fixed inset-0 flex items-center justify-center bg-white/80 z-50'}`}>
             <div className="flex flex-col items-center">
                 {/* Animación de carga */}
                 <div className="w-12 h-12 border-4 border-primary border-t-transparent rounded-full animate-spin"></div>
