24e54ee23d9b93161794810e117fb99a6cc295ee
Jorge Veliz <jorgeconsalvacion@gmail.com>
2025-07-23 12:13:46 -0500

    Add validation to view edit and remove

--- src/app/components/AppointmentList/AppointmentList.tsx
+++ src/app/components/AppointmentList/AppointmentList.tsx
@@ -8,6 +8,10 @@ import { Table } from "../Table";
 import { AppointmentListInterface } from "./AppointmentListInterface";
 
 export default function AppointmentList({ items, fetching, actions: { onRemove, onEdit } }: AppointmentListInterface) {
+
+    const mustShowEditBtn = (status: string) => (status != "completed" && status != "cancelled");
+
+
     return (
         <Table headers={['Estado', 'Paciente', 'Fecha/Hora', 'Acciones']} shadow>
             {fetching ? (<tr><td colSpan={4}><Loader onScreen={false} /></td></tr>) : ""}
@@ -17,10 +21,17 @@ export default function AppointmentList({ items, fetching, actions: { onRemove, 
                     <td className="px-6 py-2"> <AppointmentStatusBadge status={appointment.status as AppointmentStatusInterface} text={appointment.status_label} /></td>
                     <td className="px-6 py-2">{appointment.patient}</td>
                     <td className="px-6 py-2">{appointment.date} - {appointment.time} </td>
-                    <td className="px-6 py-2 flex justify-items-center mx-auto">
+                    <td className="w-full px-6 py-2 flex justify-items-center mx-auto">
                         <AppointmentChangeStatus appointmentId={appointment.id} status={appointment.status} key={appointment.id} mustUpdateList={true} />
-                        <button className="btn-secondary" aria-label="Editar" title={`Editar cita ${appointment.id}`} onClick={(e) => appointment.id !== undefined && onEdit(e, appointment.id)}> <PencilIcon /></button>
-                        <button className="btn-danger" aria-label="Eliminar" title={`Eliminar cita ${appointment.id}`} onClick={(e) => appointment.id !== undefined && onRemove(e, appointment.id)}> <TrashIcon /></button>
+                        <>
+                            {(mustShowEditBtn(appointment.status)) && (<>
+                                <button className="btn-secondary flex items-center gap-2  text-white text-xs px-3 py-1 rounded-md" aria-label="Editar" title={`Editar cita ${appointment.id}`} onClick={(e) => appointment.id !== undefined && onEdit(e, appointment.id)}> <PencilIcon className="w-4 h-4" /> Editar</button>
+                            </>)}
+                            {appointment.status !== "completed" && (<>
+                                <button className="btn-danger flex items-center gap-2  text-white text-xs px-3 py-1 rounded-md" aria-label="Eliminar" title={`Eliminar cita ${appointment.id}`} onClick={(e) => appointment.id !== undefined && onRemove(e, appointment.id)}> <TrashIcon className="w-4 h-4" /> Eliminar</button>
+                            </>)}
+                        </>
+
                     </td>
                 </tr>
             ))}
